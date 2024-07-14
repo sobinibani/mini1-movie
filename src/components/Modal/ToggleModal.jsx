0391de77917d7b9a15f039c592712e148a7fdbc2
소빈 <135595326+sobinibani@users.noreply.github.com>
2024-07-14 17:48:48 +0900

    ✨ feat: 반응형 togglemodal 기능 추가, css 적용

--- src/components/Modal/ToggleModal.jsx
+++ src/components/Modal/ToggleModal.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import { IoCloseOutline } from "react-icons/io5";
 
-const ToggleModal = ({setToggleModal}) => {
+const ToggleModal = ({toggleModalShow, setToggleModalShow}) => {
+  console.log(toggleModalShow);
+
   return (
-    <div className='ToggleModal'>
-      <div>
-        test
+    <> 
+      <div className={toggleModalShow? 'ToggleModal on' : 'ToggleModal off'} >
+        <div className='content'>
+          <div className='inner'>
+            <IoCloseOutline className='btn-close' onClick={()=>{setToggleModalShow(false)}}/>
+            <h1 className='logo'>movie</h1>
+            <div className='btnWrap'>
+              <button 
+                className='btn-login'
+                onClick={()=>{window.location.href = '/login'}}
+              >로그인</button>
+              <button 
+                className='btn-signup'
+                onClick={()=>{window.location.href = '/signup'}}
+              >회원가입</button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className={toggleModalShow? 'toggleBg on' : 'toggleBg off'} onClick={()=>{setToggleModalShow(false)}}/>
+    </>
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,43 +5,38 @@ import { IoSearch } from "react-icons/io5";
 import { MdMenu } from "react-icons/md";
 import {getAuth,signOut, onAuthStateChanged} from 'firebase/auth';
 import { useSelector, useDispatch } from 'react-redux';
-import { LoggedIn } from '../redux/actions';
-import { useAuth } from '../hooks/useAuth';
+
+import { useAuth, handleLogout } from '../hooks/useAuth';
 
 import SearchModal from './Modal/SearchModal';
 import ToggleModal from './Modal/ToggleModal';
 
-function Nav() {
-
-  // useAuth();
-  console.log(useAuth())
-  const dispatch = useDispatch();
-  const handleTestClick = () => {
-    dispatch(LoggedIn());
-  }
-  
+function Nav() {  
 
   const navigate = useNavigate();
 
   //로그인 했는지?
-  const [userData,setUserData] = useState(null);
-  // const initailUserData = localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : {}
-  const auth = getAuth();
-  const [login, setLogin] = useState(false);
+  const login = useSelector(state => state.login.isLoggedIn);
+  const userData = (useSelector(state => state.login.userData));
+  console.log(userData)
+  const dispatch = useDispatch();
+
   const [loading, setLoading] = useState(true); // 로딩 상태 추가
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLogin(true);
-        setUserData(JSON.parse(localStorage.getItem('userData')));
-      } else {
-        setLogin(false);
-      }
-      setLoading(false); // 로딩 완료
-    });
+  useAuth();
 
-  }, [auth, navigate]);
+  // useEffect(() => {
+  //   onAuthStateChanged(auth, (user) => {
+  //     if (user) {
+  //       setLogin(true);
+  //       setUserData(JSON.parse(localStorage.getItem('userData')));
+  //     } else {
+  //       setLogin(false);
+  //     }
+  //     setLoading(false); // 로딩 완료
+  //   });
+
+  // }, [auth, navigate]);
 
   
   // 검색 input 이벤트
@@ -73,17 +68,17 @@ function Nav() {
 
 
   // 로그아웃
-  const handleLogout = () => {
-    signOut(auth)
-    .then(()=>{
-      setUserData({});
-      setLogin(false);
-      localStorage.removeItem('userData');
-    })
-    .catch((error)=>{
-      console.log('error',error)
-    })
-  }
+  // const handleLogout = () => {
+  //   signOut(auth)
+  //   .then(()=>{
+  //     setUserData({});
+  //     setLogin(false);
+  //     localStorage.removeItem('userData');
+  //   })
+  //   .catch((error)=>{
+  //     console.log('error',error)
+  //   })
+  // }
 
   //반응형 - 검색 모달
   const [searchModal,setSearchModal] = useState(false);
@@ -97,7 +92,7 @@ function Nav() {
   }, [location.pathname]);
 
   //반응형 - 토글 모달
-  const [toggleModal, setToggleModal] = useState(false);
+  const [toggleModalShow, setToggleModalShow] = useState(false);
 
   return (
     <header className={` ${isScrolled ? 'scrolled' : ''}`}>
@@ -118,9 +113,8 @@ function Nav() {
               <CiSearch className='icon'/>
             </button>
           </div>
-          <div onClick={()=>{handleTestClick()}}>test</div>
         </div>
-        {loading ? null :         
+     
           <div className='btn-wrap lg-only'>
             {login ? 
               <>
@@ -132,7 +126,7 @@ function Nav() {
                     </p>
                     <button 
                     className='logout'
-                    onClick={handleLogout}
+                    onClick={()=>{handleLogout(dispatch)}}
                     >로그아웃</button>
                   </>
                 ) 
@@ -152,18 +146,20 @@ function Nav() {
               </>
             }
           </div>
-        }
+        
 
         <div className='btn-wrap lg-hidden'>
           <button className='toggle-menu lg-hidden'>
             <IoSearch onClick={()=>{setSearchModal(true)}}/>
           </button>
-          <button className='toggle-menu lg-hidden'><MdMenu /></button>
+          <button className='toggle-menu lg-hidden'>
+            <MdMenu onClick={()=>{setToggleModalShow(true)}}/>
+          </button>
         </div>
       </div>
       
       {searchModal ? <SearchModal setSearchModal={setSearchModal}/> : null}
-      {toggleModal ? <ToggleModal setToggleModal={setToggleModal}/>:null}
+      <ToggleModal toggleModalShow={toggleModalShow} setToggleModalShow={setToggleModalShow}/>
     </header>
   )
 }
