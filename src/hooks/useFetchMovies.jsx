187656fcab8001bf688a33598e062cd3c0e9694c
소빈 <135595326+sobinibani@users.noreply.github.com>
2024-07-14 17:41:35 +0900

    🐛 fix: 랜덤숫자가 받아온 무비 데이터배열의 수보다 큰 오류 수정

--- src/hooks/useFetchMovies.jsx
+++ src/hooks/useFetchMovies.jsx
@@ -2,9 +2,9 @@ import { useEffect } from "react";
 import tmdbAPI from '../api/tmdbAPI';
 
 import { useDispatch } from 'react-redux';
-import {setPopularMovies, setRandomMovies} from '../redux/movieData/action'
+import {setPopularMovies, setRandomMovies, setSearchMovies, setDetailMovies} from '../redux/movieData/action'
 
-const useFetchMovies = () => {
+export const useFetchMovies = () => {
     const dispatch = useDispatch();
 
     useEffect(()=>{
@@ -15,10 +15,10 @@ const useFetchMovies = () => {
 
                 // 설명없는 영화 제외
                 const filteredArray = response.data.results.filter((movie)=>{
-                    return movie.overview !== ""
+                    return movie.overview !== "" && movie.backdrop_path !== null;
                 });
-                const randomMovie = 
-                filteredArray[Math.floor(Math.random() * response.data.results.length)];
+                const randomNumber = Math.floor(Math.random() * filteredArray.length);
+                const randomMovie = filteredArray[randomNumber];
                 dispatch(setRandomMovies(randomMovie));
 
             } catch(error){
@@ -28,7 +28,46 @@ const useFetchMovies = () => {
 
         fetchPopularMovies();
     }, [])
-
 }
 
-export default useFetchMovies;
+export const useFetchSearchMovies = (debouncedSearchTerm) => {
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        const apiKey = process.env.REACT_APP_TMDB_API_KEY; 
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${debouncedSearchTerm}`;
+
+        const fetchSearchMovie = async()=> {
+            try{
+                const response = await tmdbAPI.get(url);
+                console.log(response)
+                const filteredArray = response.data.results.filter((movie)=>{
+                    return movie.backdrop_path !== null
+                });
+                dispatch(setSearchMovies(filteredArray));
+            } catch(error){
+                console.log(error)
+            }
+        }
+
+        fetchSearchMovie();
+    }, [debouncedSearchTerm])
+}
+
+export const useFetchDetailMovies = (id) => {
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        const fetchDetailMovie = async()=> {
+            try{
+                const response = await tmdbAPI.get(`movie/${id}`);
+                dispatch(setDetailMovies(response.data));
+            } catch(error){
+                console.log(error)
+            }
+        }
+
+        fetchDetailMovie();
+    }, [id])
+}
+
