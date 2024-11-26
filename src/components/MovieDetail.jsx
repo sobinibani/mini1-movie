f82244d2e9670ca13a01bc91374592ccb75c8789
소빈 <sobin11@naver.com>
2024-11-26 17:39:56 +0900

    🐛 영화 디테일페이지 로딩화면추가, 버그 수정

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,25 +1,27 @@
+const MovieDetail = ({ detailMovie }) => {
+  if (!detailMovie) {
+    return <div className="Loading">Loading...</div>;
+  }
 
-const MovieDetail = ({detailMovie}) => {
-    if (!detailMovie) {
-        return <div className='Loading'>Loading...</div>;
-    }
-    
-    return (
-        <div className='MovieDetail'>
-            <img
-                src={`http://image.tmdb.org/t/p/original/${detailMovie.backdrop_path}`}
-                alt={detailMovie.title}
-            />
-            <h2>{detailMovie.title}</h2>
-            <p className='movie-average'><span>평점</span>{detailMovie.vote_average}점</p>
-            <ul className='movie-genres'>
-                {detailMovie.genres.map((genres)=>{
-                    return <li>{genres.name}</li>
-                })}
-            </ul>
-            <p>{detailMovie.overview}</p>
-        </div>
-    )
-}
+  return (
+    <div className="MovieDetail">
+      <img
+        src={`http://image.tmdb.org/t/p/original/${detailMovie.backdrop_path}`}
+        alt={detailMovie.title}
+      />
+      <h2>{detailMovie.title}</h2>
+      <p className="movie-average">
+        <span>평점</span>
+        {detailMovie.vote_average}점
+      </p>
+      <ul className="movie-genres">
+        {detailMovie.genres.map((genres) => {
+          return <li>{genres.name}</li>;
+        })}
+      </ul>
+      <p>{detailMovie.overview}</p>
+    </div>
+  );
+};
 
-export default MovieDetail
+export default MovieDetail;
--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,17 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import MovieDetail from "../../components/MovieDetail"
-import {useFetchDetailMovies} from "../../hooks/useFetchMovies";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import MovieDetail from "../../components/MovieDetail";
+import { useFetchDetailMovies } from "../../hooks/useFetchMovies";
 
 const DetailPage = () => {
-    const {id} = useParams();
-    useFetchDetailMovies(id);
-    const detailMovie = useSelector(state => state.movies.detailMovies);
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true); // 로딩 상태 관리
 
-    return (
-        <MovieDetail detailMovie={detailMovie}/>
-    )
-}
+  // 영화 상세 데이터를 가져오는 커스텀 훅 호출
+  useFetchDetailMovies(id);
 
-export default DetailPage
+  // Redux에서 가져온 영화 상세 데이터
+  const detailMovie = useSelector((state) => state.movies.detailMovies);
+
+  // 데이터 로딩 상태 처리
+  useEffect(() => {
+    setLoading(true); // 새로운 id로 변경되면 로딩 상태를 true로 설정
+    if (detailMovie && Object.keys(detailMovie).length > 0) {
+      setLoading(false); // 데이터가 로드되면 로딩 상태를 false로 설정
+    }
+  }, [id, detailMovie]); // id 또는 detailMovie가 변경될 때마다 실행
+
+  // 데이터가 로드되지 않았으면 로딩 메시지 출력
+  if (loading) {
+    return <div className="Loading">Loading...</div>;
+  }
+
+  // 데이터가 로드되면 MovieDetail 컴포넌트로 전달
+  return <MovieDetail detailMovie={detailMovie} />;
+};
+
+export default DetailPage;
