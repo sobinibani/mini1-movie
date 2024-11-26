da301adfb441d7f9b24f36c36cffa8af3826454b
소빈 <sobin11@naver.com>
2024-11-26 16:31:15 +0900

    ✨ 반응형에서 toggleModal 컴포넌트 연결

--- src/components/Modal/ToggleModal.jsx
+++ src/components/Modal/ToggleModal.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React from "react";
+import { IoCloseOutline } from "react-icons/io5";
 
-const ToggleModal = ({setToggleModal}) => {
+const ToggleModal = ({ setToggleModal, userName, handleLogout }) => {
+  const toggleHandleLogout = () => {
+    setToggleModal(false);
+    handleLogout();
+  };
+
   return (
-    <div className='ToggleModal'>
-      <div>
-        test
+    <div className="ToggleModal">
+      <div className="toggleRow row1">
+        <div className="inner">
+          <button
+            onClick={() => {
+              setToggleModal(false);
+            }}
+            className="closeBtn"
+          >
+            <IoCloseOutline />
+          </button>
+        </div>
+      </div>
+      <div className="toggleRow row2">
+        <div className="inner">
+          <p className="user">
+            <span>{userName}</span>님
+          </p>
+          <button onClick={toggleHandleLogout} className="logoutBtn">
+            로그아웃
+          </button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ToggleModal
+export default ToggleModal;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,19 @@
-import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { CiSearch } from "react-icons/ci";
 import { IoSearch } from "react-icons/io5";
 import { MdMenu } from "react-icons/md";
-import { useNavigate } from 'react-router-dom';
-import {getAuth,signOut, onAuthStateChanged} from 'firebase/auth';
+import { useNavigate } from "react-router-dom";
+import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
 
-import SearchModal from './Modal/SearchModal';
-import ToggleModal from './Modal/ToggleModal';
+import SearchModal from "./Modal/SearchModal";
+import ToggleModal from "./Modal/ToggleModal";
 
 function Nav() {
   const navigate = useNavigate();
 
   //로그인 했는지?
-  const [userData,setUserData] = useState(null);
+  const [userData, setUserData] = useState(null);
   // const initailUserData = localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : {}
   const auth = getAuth();
   const [login, setLogin] = useState(false);
@@ -23,63 +23,61 @@ function Nav() {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setLogin(true);
-        setUserData(JSON.parse(localStorage.getItem('userData')));
+        setUserData(JSON.parse(localStorage.getItem("userData")));
       } else {
         setLogin(false);
       }
       setLoading(false); // 로딩 완료
     });
-
   }, [auth, navigate]);
 
-  
   // 검색 input 이벤트
-  const [searchValue, setSearchValue] = useState('');
-  const handleChange = (e)=> {
+  const [searchValue, setSearchValue] = useState("");
+  const handleChange = (e) => {
     setSearchValue(e.target.value);
-    navigate(`/search?q=${e.target.value}`)
-  }
-  
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   //스크롤
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY;
-      if (scrollTop > 50) { // 스크롤 위치가 50px 이상일 때 배경색 변경.
+      if (scrollTop > 50) {
+        // 스크롤 위치가 50px 이상일 때 배경색 변경.
         setIsScrolled(true);
       } else {
         setIsScrolled(false);
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-
 
   // 로그아웃
   const handleLogout = () => {
     signOut(auth)
-    .then(()=>{
-      setUserData({});
-      setLogin(false);
-      localStorage.removeItem('userData');
-    })
-    .catch((error)=>{
-      console.log('error',error)
-    })
-  }
+      .then(() => {
+        setUserData({});
+        setLogin(false);
+        localStorage.removeItem("userData");
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
 
   //반응형 - 검색 모달
-  const [searchModal,setSearchModal] = useState(false);
+  const [searchModal, setSearchModal] = useState(false);
   const location = useLocation();
 
   useEffect(() => {
-    const moviePage = location.pathname.startsWith('/movie/');
+    const moviePage = location.pathname.startsWith("/movie/");
     if (moviePage) {
       setSearchModal(false);
     }
@@ -87,73 +85,105 @@ function Nav() {
 
   //반응형 - 토글 모달
   const [toggleModal, setToggleModal] = useState(false);
+  const handleModal = () => {
+    if (login) {
+      setToggleModal(true);
+    } else {
+      navigate("/login");
+    }
+  };
 
   return (
-    <header className={` ${isScrolled ? 'scrolled' : ''}`}>
-      <div className='inner'>
-        <div className='left-wrap'>
+    <header className={` ${isScrolled ? "scrolled" : ""}`}>
+      <div className="inner">
+        <div className="left-wrap">
           <h1
-            className='logo'
-            onClick={()=>{window.location.href = '/'}}
-          >movie</h1>
-          <div className='search lg-only'>
+            className="logo"
+            onClick={() => {
+              window.location.href = "/";
+            }}
+          >
+            movie
+          </h1>
+          <div className="search lg-only">
             <input
-              type='text'
-              placeholder='어떤 영화를 찾으세요?'
+              type="text"
+              placeholder="어떤 영화를 찾으세요?"
               value={searchValue}
               onChange={handleChange}
             />
             <button>
-              <CiSearch className='icon'/>
+              <CiSearch className="icon" />
             </button>
           </div>
         </div>
-        {loading ? null :         
-          <div className='btn-wrap lg-only'>
-            {login ? 
+        {loading ? null : (
+          <div className="btn-wrap lg-only">
+            {login ? (
               <>
-              {userData ? 
-                (
+                {userData ? (
                   <>
-                    <p className='user-name'>
+                    <p className="user-name">
                       <span>{userData.displayName}</span> 님
                     </p>
-                    <button 
-                    className='logout'
-                    onClick={handleLogout}
-                    >로그아웃</button>
+                    <button className="logout" onClick={handleLogout}>
+                      로그아웃
+                    </button>
                   </>
-                ) 
-              : null}
+                ) : null}
               </>
-            : 
+            ) : (
               <>
-                <button 
-                  className='signup'
-                  onClick={()=>{window.location.href = '/signup'}}
-                >회원가입</button>
-                <button 
-                  className='login'
-                  onClick={()=>{window.location.href = '/login'}}
+                <button
+                  className="signup"
+                  onClick={() => {
+                    window.location.href = "/signup";
+                  }}
+                >
+                  회원가입
+                </button>
+                <button
+                  className="login"
+                  onClick={() => {
+                    window.location.href = "/login";
+                  }}
                   // onClick={handleAuth}
-                >로그인</button>
+                >
+                  로그인
+                </button>
               </>
-            }
+            )}
           </div>
-        }
+        )}
 
-        <div className='btn-wrap lg-hidden'>
-          <button className='toggle-menu lg-hidden'>
-            <IoSearch onClick={()=>{setSearchModal(true)}}/>
+        <div className="btn-wrap lg-hidden">
+          <button className="toggle-menu lg-hidden">
+            <IoSearch
+              onClick={() => {
+                setSearchModal(true);
+              }}
+            />
           </button>
-          <button className='toggle-menu lg-hidden'><MdMenu /></button>
+          <button className="toggle-menu lg-hidden">
+            <MdMenu
+              onClick={() => {
+                handleModal();
+              }}
+            />
+          </button>
         </div>
       </div>
-      
-      {searchModal ? <SearchModal setSearchModal={setSearchModal}/> : null}
-      {toggleModal ? <ToggleModal setToggleModal={setToggleModal}/>:null}
+
+      {searchModal ? <SearchModal setSearchModal={setSearchModal} /> : null}
+      {toggleModal ? (
+        <ToggleModal
+          userName={userData.displayName}
+          setToggleModal={setToggleModal}
+          handleLogout={handleLogout}
+        />
+      ) : null}
     </header>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
